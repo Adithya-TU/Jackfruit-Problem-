c842eefe0525acad1088276bea4f0f6148166a12
Adithya TU <adithyatu@gmail.com>
2025-11-29 23:05:19 +0530

    Add files via upload

--- simply.py
+++ simply.py
@@ -5,12 +5,12 @@ from tkinter import scrolledtext, simpledialog #text widget with scroll bar used
 
 # Open faqs file 
 with open("faqs.json", "r", encoding="utf-8") as f:
-    faqs = json.load(f) #reads and converts the file into a python object 
+    faqs = json.load(f) #reads and converts the file into a python object (list/dictionaries)
 
 # Save updated faqs (Learning Mode)
 def save_faqs():
     with open("faqs.json", "w", encoding="utf-8") as f:
-        json.dump(faqs, f, indent=4) #storing the python object into a json file
+        json.dump(faqs, f, indent=4) #storing the python object into a json file (does opposite of json.load)
 
 # chatbot logic (fuzzy matching + learning mode)
 def chatbot_response(user_input):
@@ -23,9 +23,9 @@ def chatbot_response(user_input):
             return answer
 
     # fuzzy match for short questions to avoid wrong matches
-    questions = list(faqs.keys())
+    questions = list(faqs.keys()) #makes a list of all the questions from the json file
     if len(user_input.split()) >= 2:
-        match = difflib.get_close_matches(user_input, questions, n=1, cutoff=0.50) #finds the strings (n amt) that are similar the entered string(based on a score bw 0 and 1)
+        match = difflib.get_close_matches(user_input, questions, n=1, cutoff=0.70) #finds the strings (n amt) that are similar the entered string(based on a score bw 0 and 1)
         if match:
             return faqs[match[0]] #return the first match found from the list
 
@@ -42,16 +42,16 @@ def chatbot_response(user_input):
 # GUI - Tkinter Interface
 #comes here first after hitting send button
 def send_message():
-    user_msg = entry.get().strip()
+    user_msg = entry.get().strip() 
     if not user_msg:
         return
 
     chatbox.insert(tk.END, "You: " + user_msg + "\n", "user")
-    entry.delete(0, tk.END)
+    entry.delete(0, tk.END) #clears the entry field from start to ending
 
-    bot_reply = chatbot_response(user_msg)
+    bot_reply = chatbot_response(user_msg) 
     chatbox.insert(tk.END, "Bot: " + bot_reply + "\n\n", "bot")
-    chatbox.see(tk.END)
+    chatbox.see(tk.END) #scrolls the chatbox viewport so the end of the text can b seen 
 
 # Build GUI
 window = tk.Tk()
@@ -68,7 +68,7 @@ chatbox.tag_config("bot", foreground="green")
 # Input Field
 entry = tk.Entry(window, font=("Arial", 12))
 entry.pack(pady=5, padx=10, fill=tk.X) #leaves that amt of pixels worth of space | to expand the entry box in the x direction
-entry.focus() #puts the cursor the the entry box on opening 
+entry.focus() #puts the cursor at the entry box on opening 
 
 # Send Button
 tk.Button(window, text="Send", font=("Arial", 12), command=send_message).pack(pady=5)
